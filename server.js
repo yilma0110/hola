ae7d73c43ca0bdb341e4866dd83bddc4c6cef6ea
yilma0110 <ruizelizaldeyilma@gmail.com>
2023-09-13 14:01:07 -0600

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.end('HOLA NIÑOS    -     EJECUTANDING DESDE LA INSTANCIA EC2');
 });
 
 server.listen(port, () => {
